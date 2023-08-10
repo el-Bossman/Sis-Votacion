afc3f8acd3401f63b1078679c8824e42e7863ca2
el-Bossman <amantedelacomida53eol@hotmail.com>
2023-08-10 11:59:27 -0400

    Add conexion.php

---
+++ conexion.php
@@ -0,0 +1,15 @@
+<?php
+	// Conexión a la base de datos 
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "votacion";
+
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	if ($conn->connect_error) {
+		die("Conexión fallida: " . $conn->connect_error);
+	}
+
+  
+?>
